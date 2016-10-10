1b2662c580571e6c82fd7365c6be78f7262a60f3
arobthechief <roberts281@live.marshall.edu>
2016-10-09 21:54:22 -0400

    git add *

--- TicketingSystem/Migrations/Configuration.cs
+++ TicketingSystem/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace TicketingSystem.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +17,24 @@ namespace TicketingSystem.Migrations
 
         protected override void Seed(TicketingSystem.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            //var manager = new UserManager<ApplicationUser>(
+            //       new UserStore<ApplicationUser>(
+            //           new ApplicationDbContext()));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    var user = new ApplicationUser()
+            //    {
+            //        UserName = string.Format("User{0}", i.ToString()),
+
+            //        // Add the following so our Seed data is complete:
+            //        FirstName = string.Format("FirstName{0}", i.ToString()),
+            //        LastName = string.Format("LastName{0}", i.ToString()),
+            //        Email = string.Format("Email{0}@Example.com", i.ToString()),
+            //    };
+            //    manager.Create(user, string.Format("Password{0}", i.ToString()));
+            //}
+
         }
     }
 }
